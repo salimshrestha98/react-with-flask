8b8ab8a7f485661b8416f2daa5ec30d30a8898a6
Salim Shrestha <26961315+salimshrestha98@users.noreply.github.com>
2023-02-05 15:12:43 +0545

    Pass data using props

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,29 @@
 import ExpenseItem from './components/ExpenseItem'
 
 function App() {
+
+  const expenses = [
+    {
+      id: 'e1',
+      title: 'Car Insurance',
+      amount: 399.99,
+      date: new Date(2023, 2, 5)
+    },
+    {
+      id: 'e2',
+      title: 'Shopping',
+      amount: 999.99,
+      date: new Date(2023, 1, 5)
+    },
+  ]
+
   return (
     <div>
-      <ExpenseItem />
+      <ExpenseItem
+        title={expenses[0].title}
+        amount={expenses[0].amount}
+        date={expenses[0].date}
+      />
     </div>
   )
 }
--- frontend/src/components/ExpenseItem.js
+++ frontend/src/components/ExpenseItem.js
@@ -1,14 +1,12 @@
 import './ExpenseItem.css';
 
-function ExpenseItem() {
+function ExpenseItem(props) {
   return (
     <div className="expense-item">
-      <div>Feb 5, 2023</div>
+      <div>{props.date.toISOString()}</div>
 	  <div className="expense-item__description">
-		<h2>
-			Car Insurance
-		</h2>
-		<div className="expense-item__price">$999.99</div>
+		<h2>{props.title}</h2>
+		<div className="expense-item__price">{props.amount}</div>
 	  </div>
     </div>
   );

4d4dbbf1c7a03afe4119cb86c195d865a0937662
Salim Shrestha <26961315+salimshrestha98@users.noreply.github.com>
2023-02-05 16:10:47 +0545

    Adding multiple components using loop

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -19,11 +19,16 @@ function App() {
 
   return (
     <div>
-      <ExpenseItem
-        title={expenses[0].title}
-        amount={expenses[0].amount}
-        date={expenses[0].date}
-      />
+      {
+        expenses.map( (expense) => (
+          <ExpenseItem
+            key={expense.id}
+            title={expense.title}
+            amount={expense.amount}
+            date={expense.date}
+          />
+        ))
+      }
     </div>
   )
 }

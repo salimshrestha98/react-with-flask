71a13a2bdd9cee4f9d2c11ed4cde088ece137238
Salim Shrestha <26961315+salimshrestha98@users.noreply.github.com>
2023-02-11 18:21:51 +0545

    Using React states

--- frontend/src/components/ExpenseItem.js
+++ frontend/src/components/ExpenseItem.js
@@ -1,3 +1,5 @@
+import React, {useState} from 'react';
+
 import './ExpenseItem.css';
 import Card from './Card';
 
@@ -5,6 +7,13 @@ function ExpenseItem(props) {
 	const month = props.date.toLocaleString('en-US', {month: 'long'});
 	const day = props.date.toLocaleString('en-US', {day: '2-digit'});
 	const year = props.date.getFullYear();
+
+	const [title, setTitle] = useState(props.title);
+
+	const clickHandler = () => {
+		console.log('clicked');
+		setTitle("Updated title");
+	}
 
   return (
     <Card className="expense-item">
@@ -14,8 +23,9 @@ function ExpenseItem(props) {
 		<div>{year}</div>
 	  </div>
 	  <div className="expense-item__description">
-		<h2>{props.title}</h2>
+		<h2>{title}</h2>
 		<div className="expense-item__price">{props.amount}</div>
+		<button onClick={clickHandler}>Change Title </button>
 	  </div>
     </Card>
   );
